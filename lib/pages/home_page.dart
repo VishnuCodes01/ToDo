ef8424daa4aa6d2a4b5ecad9bccadbca4a469648
VishnuCodes01 <hii.developer.mind@gmail.com>
2024-09-14 13:42:11 +0530

    TODO-APp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,10 +10,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List todoList = [
-    ['Learn Flutter', false],
-    ['Watch Rells', false],
-  ];
+  final _controller = TextEditingController();
+  List todoList = [];
+
+  void saveNewTask() {
+    setState(() {
+      todoList.add([_controller.text, false]);
+    });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      todoList.removeAt(index);
+      _controller.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     void checkBoxChanged(int index) {
@@ -38,8 +50,45 @@ class _HomePageState extends State<HomePage> {
             taskName: todoList[index][0],
             taskCompleted: todoList[index][1],
             onChanged: (value) => checkBoxChanged(index),
+            deleteFunction: (context) => deleteTask(index),
           );
         },
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 10.0),
+        child: Row(
+          children: [
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.notes_outlined),
+                  hintText: 'Add a new ToDo Task',
+                  filled: true,
+                  fillColor: Colors.white24,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: CupertinoColors.white,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: Colors.white70,
+                    ),
+                  ),
+                ),
+              ),
+            )),
+            FloatingActionButton(
+              onPressed: saveNewTask,
+              child: const Icon(CupertinoIcons.add),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/todo/todo_list.dart
+++ lib/todo/todo_list.dart
@@ -1,62 +1,74 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
-class TodoList extends StatefulWidget {
+class TodoList extends StatelessWidget {
   const TodoList({
     super.key,
     required this.taskName,
     required this.taskCompleted,
-    this.onChanged,
+    required this.onChanged,
+    required this.deleteFunction,
   });
 
   final String taskName;
   final bool taskCompleted;
   final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
 
-  @override
-  State<TodoList> createState() => _TodoListState();
-}
-
-class _TodoListState extends State<TodoList> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(
-        right: 20.0,
-        left: 20.0,
-        top: 20.0,
-        bottom: 0,
-      ),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.deepPurple.shade400,
-              Colors.purple.shade700,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          borderRadius: BorderRadius.circular(20),
+        padding: const EdgeInsets.only(
+          right: 20.0,
+          left: 20.0,
+          top: 20.0,
+          bottom: 0,
         ),
-        child: Row(
-          children: [
-            Checkbox(
-              value: widget.taskCompleted,
-              onChanged: widget.onChanged,
-            ),
-            Text(
-              widget.taskName,
-              style: const TextStyle(
-                color: CupertinoColors.white,
-                fontSize: 15,
-                fontWeight: FontWeight.w500,
+        child: Slidable(
+          endActionPane: ActionPane(
+            motion: const StretchMotion(),
+            children: [
+              SlidableAction(
+                onPressed: deleteFunction,
+                icon: CupertinoIcons.delete,
+                borderRadius: BorderRadius.circular(15),
               ),
+            ],
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple.shade600,
+              borderRadius: BorderRadius.circular(20),
             ),
-          ],
-        ),
-      ),
-    );
+            child: Row(
+              children: [
+                Checkbox(
+                  value: taskCompleted,
+                  onChanged: onChanged,
+                  checkColor: CupertinoColors.black,
+                  activeColor: CupertinoColors.white,
+                  side: const BorderSide(
+                    color: CupertinoColors.white,
+                  ),
+                ),
+                Text(
+                  taskName,
+                  style: TextStyle(
+                    color: CupertinoColors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                    decorationColor: CupertinoColors.white,
+                    decorationThickness: 2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
